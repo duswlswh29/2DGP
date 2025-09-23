54e18083af539a61e78befc62beb70d5e6c27b4e
duswlswh29 <yeonjin1029@naver.com>
2025-09-23 15:45:05 +0900

    프레임 크기가 달라지는 복잡한 sprite sheet로 변경

--- animation_viewer.py
+++ animation_viewer.py
@@ -3,10 +3,19 @@ from pico2d import *
 open_canvas()
 
 grass=load_image('grass.png')
+sonic=load_image('sonic-sprite1.jpg')
 
 
 def move_walk():
     print("walk")
+    frame=0
+    for x in range(0,800,10):
+        clear_canvas()
+        grass.draw(400,30)
+        sonic.clip_draw(frame*44,0,44,44,x,90)
+        update_canvas()
+        frame=(frame+1)%8
+        delay(0.05)
     pass
 
 
@@ -28,9 +37,9 @@ def move_attack():
 while True:
     grass.draw_now(400,30)
     move_walk()
-    move_run()
-    move_jump()
-    move_attack()
+    #move_run()
+    #move_jump()
+    #move_attack()
 
     pass
 
