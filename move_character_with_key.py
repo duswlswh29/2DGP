d435dc43af60ecdde83f0969b18920aa2104ab26
duswlswh29 <yeonjin1029@naver.com>
2025-09-25 18:40:53 +0900

    위아래 운동 구현

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -2,13 +2,13 @@ from pico2d import *
 
 
 open_canvas()
-grass = load_image('grass.png')
+grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running,dir
-    global x
+    global running, dir_x ,dir_y
+    global x,y
 
     # fill here
 
@@ -18,23 +18,29 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir+=1
+                dir_x+=1
             if event.key == SDLK_LEFT:
-                dir-=1
+                dir_x-=1
+            if event.key==SDLK_UP:
+                dir_y+=1
+            if event.key==SDLK_DOWN:
+                dir_y-=1
             if event.key == SDLK_ESCAPE:
                 running = False
         elif event.type== SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir-=1
+                dir_x-=1
             if event.key == SDLK_LEFT:
-                dir+=1
+                dir_x+=1
         # fill here
 
 
 running = True
 x = 800 // 2
+y=90
 frame = 0
-dir=0
+dir_x=0
+dir_y=0
 
 # fill here
 while running:
@@ -42,12 +48,13 @@ while running:
 
     # fill here
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
 
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x+=dir*5
+    x+=dir_x*5
+    y+=dir_y*5
     delay(0.05)
 
 
